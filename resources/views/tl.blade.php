031635c2e381bd6943f685485da05abdd2133af7
hayashiguchi shinya <sr0217mk1220@gmail.com>
2019-11-22 20:33:33 +0900

    update detail.blade.php,tl.blade.php for line break

--- resources/views/tl.blade.php
+++ resources/views/tl.blade.php
@@ -13,7 +13,7 @@
                     <span class="text-muted">#{{ $posts->user_id }}</span>
                 </div>
                 <div>
-                    <span>{{ $posts->content }}</span>
+                    <span>{!! nl2br(e($posts->content)) !!}</span>
                 </div>
                 @isset($posts->snippet)
                 <pre class="m-2 border border-info rounded"><code>{{ $posts->snippet }}</code></pre>
