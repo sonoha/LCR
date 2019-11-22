c245bd9621005fb3a4a150e82f4e3b3f6b6848cf
ando <gravitygravity1046@gmail.com>
2019-11-22 19:47:48 +0900

    add user_id by user_name in timeline, add reload button

--- resources/views/tl.blade.php
+++ resources/views/tl.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.app')
 @section('content')
-
+<form>
+<input type="button" value="更新" onClick="window.location.reload();">
+</form>
     <div class="container">
         @include('post', ['action' => 'tl'])
         @foreach($posts as $posts)
@@ -8,6 +10,7 @@
             <div class="col-12 my-2 py-2 bg-white shadow-sm rounded">
                 <div class="col-5 mb-1 bg-info rounded-pill" style="max-width: 200px;">
                     <span class="text-white">{{ $posts->name }}</span>
+                    <span class="text-muted">#{{ $posts->user_id }}</span>
                 </div>
                 <div>
                     <span>{{ $posts->content }}</span>
