7dc5fe1db9941578a53af1d3368814904d9fd400
飯塚　拓海 <iizuka.takumi@nifty.co.jp>
2019-11-22 19:56:55 +0900

    add design of detail page

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -1,45 +1,45 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    </head>
-    <body>
-        <div>
-          <div>
-            <p>{{ $users->name }}</p>
-          </div>
-          <div>
-            <p>{{ $posts->user_id }}</p>
-          </div>
-          <div>
-            <p>{{ $posts->content }}</p>
-          </div>
-          <div>
-            <p>{{ $posts->snippet }}</p>
-          </div>
-        </div>
-          @foreach($comments as $comments)
-            <div>
-              <div>
-                <p>{{ $comments->name }}</p>
-              </div>
-              <div>
-                <p>{{ $comments->user_id }}</p>
-              </div>
-              <div>
-                <p>{{ $comments->content }}</p>
-              </div>
-              <div>
-                <p>{{ $comments->snippet }}</p>
-              </div>
+@extends('layouts.app')
+@section('content')
+
+        <div class="col-12 mt-n3 mb-4 p-4 bg-white shadow rounded">
+            <div class="col-5 mb-1 bg-info rounded-pill" style="max-width: 200px;">
+                <span class="text-white">{{ $users->name }}</span>
+                <span class="text-muted">#{{ $posts->user_id }}</span>
             </div>
-          @endforeach
+            <div>
+                <span>{{ $posts->content }}</span>
+            </div>
+            @isset($posts->snippet)
+                <pre class="m-2 border border-info rounded"><code>{{ $posts->snippet }}</code></pre>
+            @endisset
+            <div class="text-right">
+                <span>{{ $posts->posted_at }}</span>
+            </div>
+        </div>
+    <div class="container">
+        <span>この投稿に対する返信</span>
+        @foreach($comments as $comments)
+            <div class="col-12 my-2 py-2 bg-white shadow-sm rounded">
+                <div class="col-5 mb-1 bg-info rounded-pill" style="max-width: 200px;">
+                    <span class="text-white">{{ $comments->name }}</span>
+                    <span class="text-muted">#{{ $comments->user_id }}</span>
+                </div>
+                <div>
+                    <span>{{ $comments->content }}</span>
+                </div>
+                @isset($comments->snippet)
+                    <pre class="m-2 border border-info rounded"><code>{{ $comments->snippet }}</code></pre>
+                @endisset
+                <div class="text-right">
+                    <span>{{ $comments->commented_at }}</span>
+                </div>
+            </div>
+        @endforeach
 
-          @php
+        @php
             $action = 'detail/'.$posts->post_id;
-          @endphp
+        @endphp
 
-          @include('post', ['action' => $action])
-    </body>
-</html>
+        @include('post', ['action' => $action])
+    </div>
+@endsection
