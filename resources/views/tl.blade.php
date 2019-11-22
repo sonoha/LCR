54749ef5b52c652590a0cdfbfafb21199abf3be4
ando <gravitygravity1046@gmail.com>
2019-11-22 18:12:30 +0900

    add number of comment in tl

--- resources/views/tl.blade.php
+++ resources/views/tl.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 @section('content')
 
+@include('post', ['action' => 'tl'])
     <div class="container">
-        @include('post', ['action' => 'tl'])
         @foreach($posts as $posts)
         <a href="http://localhost/detail/{{$posts->post_id}}" class="text-decoration-none text-secondary">
             <div class="col-12 my-2 py-2 bg-white shadow-sm rounded">
@@ -15,6 +15,9 @@
                 @isset($posts->snippet)
                 <pre class="m-2 border border-info rounded"><code>{{ $posts->snippet }}</code></pre>
                 @endisset
+                <div>
+                <span>{{ $posts->count }}</span>
+                </div>
                 <div class="text-right">
                     <span>{{ $posts->posted_at }}</span>
                 </div>
