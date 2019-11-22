d1425b9e71c9c7e7e878f00e90d7b59dc04b403e
飯塚　拓海 <iizuka.takumi@nifty.co.jp>
2019-11-22 13:24:31 +0900

    add design on timeline

--- resources/views/tl.blade.php
+++ resources/views/tl.blade.php
@@ -1,22 +1,24 @@
+@extends('layouts.app')
 
-@foreach($posts as $posts)
-<a href="http://localhost/detail/{{$posts->id}}">
-    <div>
-        <div>
-            <p>{{ $posts->name }}</p>
-        </div>
-        <div>
-            <p>{{ $posts->id }}</p>
-        </div>
-        <div>
-            <p>{{ $posts->content }}</p>
-        </div>
-        <div>
-            <p>{{ $posts->snippet }}</p>
-        </div>
-        <div>
-            <p>{{ $posts->posted_at }}</p>
-        </div>
+@section('content')
+    <div class="container">
+        @foreach($posts as $posts)
+        <a href="http://localhost/detail/{{$posts->id}}" class="text-decoration-none text-secondary">
+            <div class="col-12 my-2 py-2 bg-white shadow-sm rounded">
+                <div class="col-5 mb-1 bg-info rounded-pill" style="max-width: 200px;">
+                    <span class="text-white">{{ $posts->name }}</span>
+                </div>
+                <div>
+                    <span>{{ $posts->content }}</span>
+                </div>
+                @isset($posts->snippet)
+                <pre class="m-2 border border-info rounded"><code>{{ $posts->snippet }}</code></pre>
+                @endisset
+                <div class="text-right">
+                    <span>{{ $posts->posted_at }}</span>
+                </div>
+            </div>
+        </a>
+        @endforeach
     </div>
-</a>
-@endforeach
+@endsection
